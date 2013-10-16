e231bc4e4f7abba51aebac986491de275a7f8b57
Tim "Marsonge" Caillot <tim.caillot@gmail.com>
2013-10-16 14:58:20 +0200

    Finishing up..
    Modifext is now working (thanks to %%a token discovery, yay); don't
    forget to use "in" in for stuff.
    %%a is just a random token, can be replaced by %%d or %%whatever. Just
    need to put a correct "in".

--- modifext.bat
+++ modifext.bat
@@ -1,10 +1,11 @@
+cls
 @echo off
-set i=0
-set y=%1
-set x=%2
-for %%a in (*.%x%) do call :balise %%a
-:balise
-set b=%~n1
-ren %b%.%x% %y%%i%.%x%
-set /a i=%i%+1
-pause
+
+cd C:\Documents and Settings\etu\Mes documents\tp4
+
+set ext1=%1
+set ext2=%2
+
+for %%a in (*.%ext1%) do call :rename %%a
+:rename
+ren *.%ext1% *.%ext2%
