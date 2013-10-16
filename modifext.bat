d4f0dd55cba63cb150107a80d54c9ed0ed1bc7be
Tim "Marsonge" Caillot <tim.caillot@gmail.com>
2013-10-16 15:49:43 +0200

    Added condition
    Modifext needed to display an error if a parameter was missing OR no
    files could be found

--- modifext.bat
+++ modifext.bat
@@ -1,9 +1,22 @@
 cls
 @echo off
+If "%~2"=="" goto blank
+If "%~1"=="" goto blank
 
 set ext1=%1
 set ext2=%2
+if not exist *.%ext1% goto exist
 
 for %%a in (*.%ext1%) do call :rename %%a
+goto :eof
+:blank 
+echo Erreur ! Il vous manque un parametre !
+pause
+goto :eof
+:exist
+echo Erreur ! Il n'y a pas de fichier de cette extension !
+pause
+goto :eof
 :rename
 ren *.%ext1% *.%ext2%
+
